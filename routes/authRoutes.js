a789d9f3c18a7ee25104f413febed5b60e20d3d8
Abhinav <abhinav280601@gmail.com>
2023-07-24 15:43:40 +0530

    CORS applied

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,10 @@
 // routes/authRoutes.js
 const express = require("express");
 const authController = require("../controllers/authController");
+const cors = require("cors");
 
 const router = express.Router();
+router.use(cors());
 
 router.post("/loginStudent", authController.loginStudent);
 router.post("/registerStudent", authController.registerStudent);
